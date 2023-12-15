7446b52f4c4a7de106655ddf800535e351b18228
JONE POLVORA <jpolvora@gmail.com>
2023-12-15 12:14:37 -0400

    add dotenv, envalid, .env files & config

---
+++ src/env.js
@@ -0,0 +1,7 @@
+import { cleanEnv, str, num } from 'envalid'
+
+export const env = cleanEnv(process.env, {
+  PORT: num(),
+  //API_KEY: str(),
+  NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging'] }),
+})
--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import Api from "./api.js"
-import Server from "./server.js"
+import 'dotenv/config.js'
+import { env } from './env.js'
+import Api from './api.js'
+import Server from './server.js'
 
 async function start() {
   const api = new Api()
-  const port = process.env.PORT || 3000
-  const server = new Server(api, Number(port))
+  const server = new Server(api, env.PORT)
   try {
-    const httpServer = await server.listen()
-    httpServer.on("connection", function () {
-      console.log("someone is connected")
-    })
-    console.log("server listening")
+    await server.listen()
   } catch (error) {
-    throw new Error("error on trying to run Server:" + error)
+    throw new Error('error on trying to run Server:' + error)
   }
 }
 
